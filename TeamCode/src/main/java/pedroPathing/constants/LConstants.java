af85a19ae51e0c33cb4de92e0cd3cf75537a9772
abdulmalekbaitulmal <malekbaitulmal@gmail.com>
2025-03-02 12:35:33 +0200

    Say Hi To Pedro Pathing

---
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -0,0 +1,26 @@
+package pedroPathing.constants;
+
+import com.pedropathing.localization.*;
+import com.pedropathing.localization.constants.*;
+
+public class LConstants {
+    static {
+        DriveEncoderConstants.forwardTicksToInches = 1;
+        DriveEncoderConstants.strafeTicksToInches = 1;
+        DriveEncoderConstants.turnTicksToInches = 1;
+
+        DriveEncoderConstants.robot_Width = 14.33071;
+        DriveEncoderConstants.robot_Length = 10.43307;
+
+        DriveEncoderConstants.leftFrontEncoderDirection = Encoder.FORWARD;
+        DriveEncoderConstants.rightFrontEncoderDirection = Encoder.REVERSE;
+        DriveEncoderConstants.leftRearEncoderDirection = Encoder.FORWARD;
+        DriveEncoderConstants.rightRearEncoderDirection = Encoder.REVERSE;
+
+
+    }
+}
+
+
+
+
